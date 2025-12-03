7f73311f40e77310e81a7172999d52bd665a73db
Dilaefendioglu <dilaefendioglu@gmail.com>
2025-12-03 23:29:05 +0300

    feat: integrate Firebase Authentication
    - Added Firebase app configuration
    - Set up Authentication service (email/password)
    - Implemented modular structure: firebase.js, auth.js, ui.js
    - Completed register and login flow

---
+++ js/ui.js
@@ -0,0 +1,50 @@
+// REGISTER KISMI
+import { register, login } from "./auth.js";
+import { auth } from "./firebase.js";
+import { updateProfile } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+// REGISTER BUTONU
+const registerBtn = document.getElementById("registerBtn");
+
+if (registerBtn) {
+  registerBtn.addEventListener("click", () => {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    register(email, password)
+      .then(() => {
+        // Kullanıcı profilini güncelle (ad soyad)
+        updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        alert("Kayıt başarılı!");
+        window.location.href = "login.html";
+      })
+      .catch((err) => alert(err.message));
+  });
+}
+
+// LOGIN BUTONU
+const loginBtn = document.getElementById("loginBtn");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    login(email, password)
+      .then(() => {
+        alert("Giriş başarılı!");
+        window.location.href = "index.html"; // başarılı giriş sonrası ana sayfa
+      })
+      .catch((err) => {
+        if (err.code === "auth/invalid-credential") {
+          alert("Email veya şifre hatalı.");
+        } else {
+          alert(err.message);
+        }
+      });
+  });
+}
