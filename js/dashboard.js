4893817938728e80781d3c998de6a3ab4b1db3c5
Dila Efendioğlu <dilaefendioglu@gmail.com>
2026-01-27 17:17:51 +0300

    implement favorites screen and enhance dashboard design

--- js/dashboard.js
+++ js/dashboard.js
@@ -25,53 +25,21 @@ const closeBtn = document.getElementById("closeBtn");
 export function openDetail(imageUrl)  {
   detailImage.src = imageUrl;
   detailModal.classList.remove("hidden");
-  document.body.style.overflow = "hidden"; 
+  document.body.style.overflow = "hidden";
  }
   
  if(closeBtn){
   closeBtn.addEventListener("click" , () =>{
    detailModal.classList.add("hidden");
    detailImage.src = "";
+   document.body.style.overflow = "auto";
   
   });
+ } 
+
+ const favBtn = document.getElementById("favBtn");
+ if (favBtn) {
+  favBtn.addEventListener("click" ,() => {
+    window.location.href = "favorites.html"
+  } )
  }
-
-import { fetchImages } from "./api.js";
-import { renderImages } from "./render.js";
-
-
-const searchInput = document.getElementById("searchInput");
-
-if (searchInput) {
-  searchInput.addEventListener("keydown", async (event) => {
-    if (event.key === "Enter") {
-      const query = searchInput.value.trim();
-      if (!query) return;
-
-          sessionStorage.setItem("lastSearch", query); //aramayı saklıyoruz.
-
-      const images = await fetchImages(query);
-      renderImages(images);
-    }
-  });
-}
-
-const detailModal = document.getElementById("detailModal");
-const detailImage = document.getElementById("detailImage");
-const closeBtn = document.getElementById("closeBtn");
-
-export function openDetail(imageUrl)  {
-  detailImage.src = imageUrl;
-  detailModal.classList.remove("hidden");
-  document.body.style.overflow = "hidden"; 
- }
-  
- if(closeBtn){
-  closeBtn.addEventListener("click" , () =>{
-   detailModal.classList.add("hidden");
-   detailImage.src = "";
-  
-  });
- }
-
- 
